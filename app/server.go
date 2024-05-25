be1f3fa973e077ad2ee41ee806556fa33e2b1313
Robert Oles <spamcislo2@hotmail.sk>
2024-05-25 12:15:53 +0200

    Handle multiple requests

--- app/server.go
+++ app/server.go
@@ -7,11 +7,7 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -25,10 +21,9 @@ func main() {
 		os.Exit(1)
 	}
 	defer conn.Close()
-	buf := make([]byte, 1024)
-	_, _ = conn.Read(buf)
-	// if err != nil {
-	// 	os.Exit(1)
-	// }
-	conn.Write([]byte("+PONG\r\n"))
+	for {
+		buf := make([]byte, 1024)
+		_, _ = conn.Read(buf)
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
