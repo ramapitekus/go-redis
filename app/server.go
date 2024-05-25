8efe82e8ebc6429aa66ec6e3d0fc63da97ce7344
Robert Oles <spamcislo2@hotmail.sk>
2024-05-25 17:02:54 +0200

    Add Get/Set

--- app/server.go
+++ app/server.go
@@ -20,12 +20,15 @@ type ParsedElement struct {
 	Type   int
 }
 
+var dataTypeMap = map[string]int{
+	"*": ARRAY,
+	"$": STR,
+	"+": SIMPLE_STR,
+}
+
+var KeyValueStore = map[string]string{}
+
 func main() {
-	data_type_map := map[string]int{
-		"*": ARRAY,
-		"$": STR,
-		"+": SIMPLE_STR,
-	}
 
 	fmt.Println("Logs from your program will appear here!")
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -41,92 +44,104 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn, data_type_map)
+		go handleConnection(conn)
 	}
 }
 	
-func handleConnection(conn net.Conn, data_type_map map[string]int) {
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
 		buf := make([]byte, 1024)
-		n, _ := conn.Read(buf)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading from connection:", err)
+			return
+		}
+
 		request := string(buf[:n])
-		result, _ := parseElement(request, data_type_map)
+		result, _ := parseElement(request)
 
 		if result.Type == ARRAY {
 			query := result.Array // e.g. ["ECHO", "hey"]
 			command := query[0]
-			if command.String == "ECHO" {
+			switch command.String{
+			case "ECHO":
 				content := query[1].String
 				response := fmt.Sprintf("$%d\r\n%s\r\n", len(content), query[1].String)
 				conn.Write([]byte(response))	
-			}
-			if command.String == "PING" {
+			case "PING":
 				conn.Write([]byte("+PONG\r\n"))
+			case "SET":
+				key, value := query[1].String, query[2].String
+				KeyValueStore[key] = value
+				conn.Write([]byte("+OK\r\n"))
+			case "GET":
+				key := query[1].String
+				if value, ok := KeyValueStore[key]; ok {
+					conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)))
+				} else {
+					conn.Write([]byte("$-1\r\n"))
+				}
 			}
+			
 		}
 	}
 }
 
 
-func parseElement(element string, data_type_map map[string]int) (ParsedElement, int) {
-	// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
-	// ["ECHO", "hey"]
-	data_type := string(element[0])
+func parseElement(element string) (ParsedElement, int) {
+	dataType := string(element[0])
 	
-	op := data_type_map[data_type]
-	if op == ARRAY {
-		return parseArray(element, data_type_map)
-	}
-	if op == STR {
-		return parseString(element, data_type_map)
-	}
-	if op == SIMPLE_STR {
-		return parseSimpleString(element, data_type_map)
+	op := dataTypeMap[dataType]
+	switch op {
+	case ARRAY:
+		return parseArray(element)
+	case STR:
+		return parseString(element)
+	case SIMPLE_STR:
+		return parseSimpleString(element)
 	}
 	return ParsedElement{}, -1
 }
 
-func parseSimpleString(element string, data_type_map map[string]int) (ParsedElement, int) {
+func parseSimpleString(element string) (ParsedElement, int) {
 	return ParsedElement{String: strings.TrimRight(element[1:], "\r\n"), Type: SIMPLE_STR}, len(element)
 }
 
-func parseString(element string, data_type_map map[string]int) (ParsedElement, int) {
-	// $4\r\nECHO\r\n$3\r\nhey\r\n
-	split_element := strings.SplitN(element, "\r\n", 2)
-	length_str, remainder := split_element[0][1:], split_element[1]
+func parseString(element string) (ParsedElement, int) {
+	splitElement := strings.SplitN(element, "\r\n", 2)
+	lengthString, body := splitElement[0][1:], splitElement[1]
 
-	length, err := strconv.Atoi(length_str)
+	length, err := strconv.Atoi(lengthString)
 	if err != nil {
 		fmt.Println("Failed to parse STR Data type - could not convert length of the array to int.")
 		os.Exit(1)
 	}
-	return ParsedElement{String: remainder[:length], Type: STR}, length + len(length_str) + 2 + 4 - 1 // 2 for types, 4 for \r\n, -1 length to index
+	return ParsedElement{String: body[:length], Type: STR}, length + len(lengthString) + 2 + 4 - 1 // 2 for types, 4 for \r\n, -1 length to fix index
 	
 }
 
-// *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
-func parseArray(element string, data_type_map map[string]int) (ParsedElement, int) {
-	split_element := strings.SplitN(element, "\r\n", 2)
-	array_length_str, remainder := split_element[0][1:], split_element[1]
-	array_length, err := strconv.Atoi(array_length_str)
+func parseArray(element string) (ParsedElement, int) {
+	splitElement := strings.SplitN(element, "\r\n", 2)
+	arrayLengthString, body := splitElement[0][1:], splitElement[1]  // 0[:1] - all except the first special sign
+	arrayLength, err := strconv.Atoi(arrayLengthString)
 	if err != nil {
 		fmt.Println("Failed to parse ARRAY Data type - could not convert length of the array to int.")
 		os.Exit(1)
 	}
 
-	elements_array := make([]ParsedElement, array_length)
-	end_index_cum := 0
-	var parsed_value ParsedElement
-	var end_index int
-	for element_index := 0; element_index < array_length; element_index++ {
-		parsed_value, end_index = parseElement(remainder[end_index_cum:], data_type_map)
+	elementsArray := make([]ParsedElement, arrayLength)
+	endIndexCum := 0
+	var parsedValue ParsedElement
+	var endIndex int
+	for elementIndex := 0; elementIndex < arrayLength; elementIndex++ {
+		parsedValue, endIndex = parseElement(body[endIndexCum:])
 		if err != nil {
 			os.Exit(1)
 		}
-		elements_array[element_index] = parsed_value
-		end_index_cum += end_index
+		elementsArray[elementIndex] = parsedValue
+		endIndexCum += endIndex
 	}
-	return ParsedElement{Array: elements_array, Type: ARRAY}, len(array_length_str) + 4 + 1  // 1 for type, 4 for \r\n
+	return ParsedElement{Array: elementsArray, Type: ARRAY}, arrayLength + len(arrayLengthString) + 2 + 4 - 1 // 2 for types, 4 for \r\n, -1 length to fix index
 	
 }
