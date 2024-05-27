f069e52fa99959b9562c04390feeb37e28ffcf1c
Robert Oles <spamcislo2@hotmail.sk>
2024-05-27 23:56:56 +0200

    Change to hex

--- app/server.go
+++ app/server.go
@@ -105,9 +105,11 @@ func handlePsync(conn net.Conn, command []RedisElement) error {
 	if err != nil {
 		panic("Replica did not accept")
 	}
-	// payload := RedisElement{String: "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==", Type: STR}.ToString()
-	conn.Write([]byte(fmt.Sprint("$", len("UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="), "\r\n")))
-	conn.Write([]byte("UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="))
+	_, err = conn.Write([]byte(fmt.Sprint("$", len("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"), "\r\n")))
+	if err != nil {
+		panic("fucker")
+	}
+	conn.Write([]byte("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"))
 	return nil
 }
 	
